2393cd0e1124e6c2e89c561e15ba651deae1593d
Shograti <petitberghienleo@gmail.com>
2025-06-17 14:49:23 +0200

    Added details to the Homepage

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.css"
+import searchIcon from "../../assets/search-icon.svg"
 
 function Home() {
     return (
@@ -6,7 +7,10 @@ function Home() {
             <div className={styles.search}>
                 <h1>Search</h1>
                 <p>Seach high-resolution images from Unsplash</p>
-                <input placeholder="Enter your keywords..." type="text" />
+                <div className={styles.search_input}>
+                    <input placeholder="Enter your keywords..." type="text" />
+                    <img src={searchIcon} alt="" className={styles.search_icon} />
+                </div>
             </div>
         </div>
     )
